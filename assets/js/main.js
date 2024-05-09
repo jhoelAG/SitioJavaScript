bd061bc24afb3eb6d6c25bdf2b1e97a3eedb4783
Franco Morales Castro <franco.morales@outlook.com>
2024-05-08 22:24:02 -0400

    feat: inicio javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,4 +65,120 @@ function javaScriptBegin() {
     // serializar: convertir un objeto javascrit a string
     var salida = JSON.stringify(persona);
     
+    // bloques de condicion
+    // == es para igualdad
+    // === es para igualdad estricta
+    // && para representar el and
+    // || para representar el or
+    // !== para representar el distinto
+    // ! para representar el not
+
+    if (2 == '2') {
+        // bloque de codigo verdadero
+    } else {
+        // bloque de codigo falso
+    }
+
+    if (2 == '2') {
+        // bloque de codigo verdadero
+    } else if (3 == 3) {
+        // segundo bloque de codigo verdadero
+    } else {
+        // bloque de codigo falso
+    }
+
+    // Trabajo con objetos
+    let objeto1 = {
+        id: 1,
+        nombre: "Angela",
+        apellido: "Castro",
+        showFullName: function () {
+            console.log(`${nombre} ${apellido}`)
+        }
+    }
+
+    objeto1.showFullName()
+
+    class Persona {
+        constructor(nombre, apellido) {
+            this.nombre = nombre;
+            this.apellido = apellido
+        }
+
+        saludar() {
+            console.log(`Hola, mi nombre es ${this.nombre}`);
+        }
+    }
+    let persona1 = new Persona("Pedro", "Rojas");
+
+    // Herencia
+    class Empleado extends Persona {
+        constructor(nombre, apellido, edad, puesto) {
+            super(nombre, apellido);
+            this.edad = edad;
+            this.puesto = puesto;
+        }
+
+        presentar() {
+            console.log(`Hola, mi nombre es ${this.nombre} y soy ${this.puesto}`);
+        }
+    }
+
+    // Encapsulamiento: el principio de ocultar detalles internos de un objeto,
+    // y exponer solo lo necesario.
+    class CuentaBancaria {
+        #saldo; // con # protejemos el atributo
+
+        constructor (saldoInicial) {
+            this.#saldo = saldoInicial;
+        }
+
+        depositar (cantidad) {
+            if (cantidad > 0) {
+                this.#saldo += cantidad;
+            }
+        }
+
+        verSaldo() {
+            console.log(`El saldo actual es: ${this.#saldo}`);
+        }
+    }
+
+    let cuenta1 = new CuentaBancaria(100000);
+    
+    // Polimorfismo: permite qye diferentes clases tengan metodos con el mombre
+    // pero con comportamientos diferentes
+    class Animal {
+        hablar() {
+            console.log('Un sonido de animal');
+        }
+    }
+
+    class Perro extends Animal {
+        hablar() {
+            console.log('Guau Guau');
+        }
+    }
 }
+
+// arrow functions
+const suma = (a, b) => {
+    total = a + b;
+    return total;
+}
+
+const suma2 = (a, b) => a + b;
+
+// Los operadores ternarios
+// const resultado = a === b ? 'Si' : 'No';
+
+
+// Asignar evento a un elemento o control del documento web
+let botonSumar = document.getElementById("boton_sumar");
+console.log(botonSumar);
+
+botonSumar.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('Hice click!!!');
+});
+
